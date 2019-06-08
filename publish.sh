5a7e7b0ab268fb1f4e0b0f8a4eba4c1962bf8b6a
Robert Sundström <robert.sundstrom@outlook.com>
2019-06-08 23:22:06 +0200

    Date format in posts

--- publish.sh
+++ publish.sh
@@ -1,1 +1,8 @@
-dotnet publish "/Users/robert/Projects/PersonalSite/src/PersonalSite/PersonalSite.csproj" -c Release
+pushd "/Users/robert/Projects/PersonalSite/src/PersonalSite"
+dotnet publish -c Release
+pushd "bin/Release/netstandard2.0/publish/PersonalSite"
+git add .
+git commit -m "Update app"
+git push
+popd
+popd
