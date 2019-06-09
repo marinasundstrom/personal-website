0b187a24649282fb10cc20af3ffb716bf7ee8462
Robert Sundström <robert.sundstrom@outlook.com>
2019-06-09 10:51:25 +0200

    Add support for viewing revision

--- src/BlogEngine/Content/Providers/GitHubClient.cs
+++ src/BlogEngine/Content/Providers/GitHubClient.cs
@@ -11,6 +11,7 @@ namespace BlogEngine.Content.Providers
     {
         Task<IEnumerable<Content>> GetContentAsync(string path);
         Task<string> GetRawContentAsync(string path);
+        Task<IEnumerable<CommitDto>> GetCommitsAsync(string path);
     }
 
     public class GitHubClient : IGitHubClient
@@ -30,8 +31,8 @@ namespace BlogEngine.Content.Providers
         {
             var configuration = configurationManager.Configuration;
             var contentUri = GetUserRawContentUri(
-                configuration.GetSection("GitHub").GetValue<string>("User"), 
-                configuration.GetSection("GitHub").GetValue<string>("Repository"), 
+                configuration.GetSection("GitHub").GetValue<string>("User"),
+                configuration.GetSection("GitHub").GetValue<string>("Repository"),
                 configuration.GetSection("GitHub").GetValue<string>("Branch"), path);
 
             return await httpClient.GetStringAsync(contentUri);
@@ -56,7 +57,24 @@ namespace BlogEngine.Content.Providers
 
         private static string GetUserContentUri(string user, string repository, string contentPath)
         {
-            return $"https://api.github.com/repos/{user}/{repository}/contents/{contentPath}";
+            return GetUri(user, repository, $"contents/{contentPath}");
+        }
+
+        private static string GetUri(string user, string repository, string path)
+        {
+            return $"https://api.github.com/repos/{user}/{repository}/{path}";
+        }
+
+        public async Task<IEnumerable<CommitDto>> GetCommitsAsync(string path)
+        {
+
+            var configuration = configurationManager.Configuration;
+            var contentUri = GetUri(
+                configuration.GetSection("GitHub").GetValue<string>("User"),
+                configuration.GetSection("GitHub").GetValue<string>("Repository"), $"commits?path={path}");
+
+            var content = await httpClient.GetStringAsync(contentUri);
+            return JsonConvert.DeserializeObject<CommitDto[]>(content);
         }
     }
 
@@ -83,4 +101,150 @@ namespace BlogEngine.Content.Providers
         public string html { get; set; }
     }
 
+    public partial class CommitDto
+    {
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("sha")]
+        public string Sha { get; set; }
+
+        [JsonProperty("node_id")]
+        public string NodeId { get; set; }
+
+        [JsonProperty("html_url")]
+        public Uri HtmlUrl { get; set; }
+
+        [JsonProperty("comments_url")]
+        public Uri CommentsUrl { get; set; }
+
+        [JsonProperty("commit")]
+        public Commit Commit { get; set; }
+
+        [JsonProperty("author")]
+        public WelcomeAuthor Author { get; set; }
+
+        [JsonProperty("committer")]
+        public WelcomeAuthor Committer { get; set; }
+
+        [JsonProperty("parents")]
+        public Tree[] Parents { get; set; }
+    }
+
+    public partial class WelcomeAuthor
+    {
+        [JsonProperty("login")]
+        public string Login { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("node_id")]
+        public string NodeId { get; set; }
+
+        [JsonProperty("avatar_url")]
+        public Uri AvatarUrl { get; set; }
+
+        [JsonProperty("gravatar_id")]
+        public string GravatarId { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("html_url")]
+        public Uri HtmlUrl { get; set; }
+
+        [JsonProperty("followers_url")]
+        public Uri FollowersUrl { get; set; }
+
+        [JsonProperty("following_url")]
+        public string FollowingUrl { get; set; }
+
+        [JsonProperty("gists_url")]
+        public string GistsUrl { get; set; }
+
+        [JsonProperty("starred_url")]
+        public string StarredUrl { get; set; }
+
+        [JsonProperty("subscriptions_url")]
+        public Uri SubscriptionsUrl { get; set; }
+
+        [JsonProperty("organizations_url")]
+        public Uri OrganizationsUrl { get; set; }
+
+        [JsonProperty("repos_url")]
+        public Uri ReposUrl { get; set; }
+
+        [JsonProperty("events_url")]
+        public string EventsUrl { get; set; }
+
+        [JsonProperty("received_events_url")]
+        public Uri ReceivedEventsUrl { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("site_admin")]
+        public bool SiteAdmin { get; set; }
+    }
+
+    public partial class Commit
+    {
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("author")]
+        public CommitAuthor Author { get; set; }
+
+        [JsonProperty("committer")]
+        public CommitAuthor Committer { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("tree")]
+        public Tree Tree { get; set; }
+
+        [JsonProperty("comment_count")]
+        public long CommentCount { get; set; }
+
+        [JsonProperty("verification")]
+        public Verification Verification { get; set; }
+    }
+
+    public partial class CommitAuthor
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+    }
+
+    public partial class Tree
+    {
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("sha")]
+        public string Sha { get; set; }
+    }
+
+    public partial class Verification
+    {
+        [JsonProperty("verified")]
+        public bool Verified { get; set; }
+
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+
+        [JsonProperty("signature")]
+        public object Signature { get; set; }
+
+        [JsonProperty("payload")]
+        public object Payload { get; set; }
+    }
 }
