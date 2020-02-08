ac4b95e0c2d5a4f17db9e97dd9c59ea5dc2f60ef
Robert Sundström <robert.sundstrom@outlook.com>
2020-02-08 13:03:47 +0100

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 pushd "src/PersonalSite"
 dotnet publish -c Release
-pushd "bin/Release/netstandard2.0/publish/PersonalSite/dist"
+pushd "bin/Release/netstandard2.1/publish/PersonalSite/dist"
 git add .
 git commit -m "Update app"
 git push
